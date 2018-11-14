2074a31c3c9e1c453ce36a8703de27670e838686
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2018-11-14 06:09:52 +0100

    Initial commit

--- email-finder.py
+++ email-finder.py
@@ -7,7 +7,6 @@
 
 import re
 import sys
-import validate_email
 
 # Collect first name, last name, domain
 try:
@@ -58,18 +57,35 @@ regex_check(domain_regex, domain_name)
 
 def email_list(first, last, domain):
     list = []
-    list.append("test")
-    return list
+
+    list.append(first + '@' + domain)                    # first@example.com
+    list.append(first[0] + '@' + domain)                 # f@example.com
+    list.append(first[0] + last + '@' + domain)          # flast@example.com
+    list.append(first[0] + '.' + last + '@' + domain)    # f.last@example.com
+    list.append(first[0] + '_' + last + '@' + domain)    # f_last@example.com
+    list.append(first[0] + '-' + last + '@' + domain)    # f-last@example.com
+    list.append(first[0] + '-' + last + '@' + domain)    # first@example.com
+    list.append(first + last + '@' + domain)             # firstlast@example.com
+    list.append(first + '.' + last + '@' + domain)       # first.last@example.com
+    list.append(first + '_' + last + '@' + domain)       # first_last@example.com
+    list.append(first + '-' + last + '@' + domain)       # first-last@example.com
+    list.append(first[0] + last[0] + '@' + domain)       # fl@example.com
+    list.append(first[0] + '.' + last[0] + '@' + domain) # f.l@example.com
+    list.append(first[0] + '-' + last[0] + '@' + domain) # f_l@example.com
+    list.append(first[0] + '-' + last[0] + '@' + domain) # f-l@example.com
+    list.append(first + last[0] + '@' + domain)          # fistl@example.com
+    list.append(first + '.' + last[0] + '@' + domain)    # first.l@example.com
+    list.append(first + '_' + last[0] + '@' + domain)    # fist_l@example.com
+    list.append(first + '-' + last[0] + '@' + domain)    # fist-l@example.com
+    list.append(last + '@' + domain)                     # last@example.com
+    list.append(last[0] + '@' + domain)                  # l@example.com
+
+    return(list)
 
 # Find combinations for given input
-email_list(first_name, last_name, domain_name)
-
-print(email_list)
+email_list = email_list(first_name, last_name, domain_name)
 
 # TODO: Validate them one by one
 
+
 # TODO: If all are valid, give a catch-all warning
-
-# TODO: List all valid email addresses
-
-# TODO (OPTIONAL): Create file with email(s) or copy to clipboard
